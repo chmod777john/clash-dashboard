039a74ec8d6ec06ae9f19b39dde4e74624fca2f8
jas0ncn <me@ijason.cc>
2018-10-27 23:35:51 +0800

    Add: overview

--- src/containers/Overview/index.tsx
+++ src/containers/Overview/index.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react'
-import { inject, observer } from 'mobx-react'
-import { storeKeys } from '@lib/createStore'
-import { BaseRouterProps } from '@models'
+const logo = require('@assets/LOGO-fixing.svg')
 
-@inject(...storeKeys)
-@observer
-export default class Overview extends React.Component<BaseRouterProps, {}> {
-
-    componentDidMount () {
-        // here can access stores
-        console.log(this.props)
-    }
-
+export default class Overview extends React.Component<{}, {}> {
     render () {
-        return 'Overview'
+        return (
+            <div style={{
+                height: '90vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                opacity: 0.3
+            }}>
+                <img src={logo} alt="Logo" style={{
+                    width: 200
+                }}/>
+
+                <h1 style={{ color: '#54759A', marginTop: 20 }}>Coming Soon...</h1>
+            </div>
+        )
     }
 }
