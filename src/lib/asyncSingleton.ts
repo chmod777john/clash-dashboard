1686f50b379238803ae85521bec5611addd48680
Dreamacro <305009791@qq.com>
2019-09-30 16:51:49 +0800

    Chore: improve createAsyncSingleton

--- src/lib/asyncSingleton.ts
+++ src/lib/asyncSingleton.ts
@@ -1,29 +1,15 @@
 export function createAsyncSingleton<T> (fn: () => Promise<T>): () => Promise<T> {
     let promise: Promise<T> | null = null
-    let instance: T | null = null
 
-    async function fetch () {
+    return async function () {
         if (promise) {
             return promise
         }
-
         promise = fn()
         return promise
-            .then(r => {
-                promise = null
-                return r
-            })
             .catch(e => {
                 promise = null
-                return e
+                throw e
             })
-    }
-
-    return async function () {
-        if (instance) {
-            return instance
-        }
-
-        return fetch()
     }
 }
