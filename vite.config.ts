9650625942b71563e9827804e0a4b990071bca59
Dreamacro <8615343+Dreamacro@users.noreply.github.com>
2021-10-01 20:12:25 +0800

    Chore: use pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -4,32 +4,35 @@ import { VitePWA } from 'vite-plugin-pwa'
 import windiCSS from 'vite-plugin-windicss'
 import tsConfigPath from 'vite-tsconfig-paths'
 
-export default defineConfig({
-    plugins: [
-        react({ babel: { compact: false } }),
-        tsConfigPath(),
-        windiCSS(),
-        VitePWA({
-            injectRegister: 'inline',
-            manifest: {
-                icons: [{
-                    src: '//cdn.jsdelivr.net/gh/Dreamacro/clash-dashboard/src/assets/Icon.png',
-                    sizes: '512x512',
-                    type: 'image/png',
-                }],
-                start_url: '/',
-                short_name: 'Clash Dashboard',
-                name: 'Clash Dashboard',
-            },
-        }),
-    ],
-    base: './',
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@use "sass:math"; @import "src/styles/variables.scss";',
+export default defineConfig(
+    env => ({
+        plugins: [
+            // only use react-fresh
+            env.mode === 'development' && react(),
+            tsConfigPath(),
+            windiCSS(),
+            VitePWA({
+                injectRegister: 'inline',
+                manifest: {
+                    icons: [{
+                        src: '//cdn.jsdelivr.net/gh/Dreamacro/clash-dashboard/src/assets/Icon.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    }],
+                    start_url: '/',
+                    short_name: 'Clash Dashboard',
+                    name: 'Clash Dashboard',
+                },
+            }),
+        ],
+        base: './',
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "sass:math"; @import "src/styles/variables.scss";',
+                },
             },
         },
-    },
-    build: { reportCompressedSize: false },
-})
+        build: { reportCompressedSize: false },
+    }),
+)
